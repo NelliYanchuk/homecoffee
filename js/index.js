4e91e981639f4c09266d7c6e03a38842c7cc1a39
NelliYanchuk <yanchuk.nelli@gmail.com>
2024-10-11 00:17:51 +0200

    format

--- js/index.js
+++ js/index.js
@@ -1,23 +1,23 @@
 //const requestURL = '../json/coffees.json'
 const requestURL = 'http://localhost:3000/coffees'
 
-async function fetchCoffeeJson() {
-    const response = await fetch(requestURL)
-    const coffees = await response.json()
-    return coffees
+async function fetchCoffeeJson () {
+  const response = await fetch(requestURL)
+  const coffees = await response.json()
+  return coffees
 }
 
 fetchCoffeeJson().then(coffeeArray => {
-    for (let index = 0; index < coffeeArray.length; index++) {
-        let id = coffeeArray[index].id
-        let title = coffeeArray[index].title
-        let description = coffeeArray[index].description
-        let country = coffeeArray[index].country
-        let price = coffeeArray[index].price
-        let currency = coffeeArray[index].currencyFormat
-        let image = coffeeArray[index].image
+  for (let index = 0; index < coffeeArray.length; index++) {
+    let id = coffeeArray[index].id
+    let title = coffeeArray[index].title
+    let description = coffeeArray[index].description
+    let country = coffeeArray[index].country
+    let price = coffeeArray[index].price
+    let currency = coffeeArray[index].currencyFormat
+    let image = coffeeArray[index].image
 
-        coffeeSection.innerHTML += `
+    coffeeSection.innerHTML += `
 
         <div class="card" style="width: 18rem;">
         <img src="${image}" class="card-img-top" alt="${title}">
@@ -29,5 +29,5 @@ fetchCoffeeJson().then(coffeeArray => {
         </div>
 
         `
-    }
+  }
 })
