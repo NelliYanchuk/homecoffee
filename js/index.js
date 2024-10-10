6b82d8b984330ae62edcdf4ba6def647cfca225e
NelliYanchuk <yanchuk.nelli@gmail.com>
2024-10-11 00:11:53 +0200

    json images, js part in html, css and js files

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
-const requestURL = '../json/coffees.json'
+//const requestURL = '../json/coffees.json'
+const requestURL = 'http://localhost:3000/coffees'
 
 async function fetchCoffeeJson() {
     const response = await fetch(requestURL)
@@ -6,18 +7,18 @@ async function fetchCoffeeJson() {
     return coffees
 }
 
-fetchCoffeeJson().then(coffee => {
-    for (let index = 0; index < coffee.coffees.length; index++) {
-        let id = coffee.coffees[index].id
-        let title = coffee.coffees[index].title
-        let description = coffee.coffees[index].description
-        let country = coffee.coffees[index].country
-        let price = coffee.coffees[index].price
-        let currency = coffee.coffees[index].currencyFormat
-        let image = coffee.coffees[index].image
+fetchCoffeeJson().then(coffeeArray => {
+    for (let index = 0; index < coffeeArray.length; index++) {
+        let id = coffeeArray[index].id
+        let title = coffeeArray[index].title
+        let description = coffeeArray[index].description
+        let country = coffeeArray[index].country
+        let price = coffeeArray[index].price
+        let currency = coffeeArray[index].currencyFormat
+        let image = coffeeArray[index].image
 
         coffeeSection.innerHTML += `
-            
+
         <div class="card" style="width: 18rem;">
         <img src="${image}" class="card-img-top" alt="${title}">
         <div class="card-body">
